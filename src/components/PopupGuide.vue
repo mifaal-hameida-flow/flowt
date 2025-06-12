91b888fce2b112a6e01bd0e4835e2e6f1270d0a3
GiliLikesOatmeal <gili.gordon12@gmail.com>
2025-06-12 10:47:51 +0300

    sacaaf

--- src/components/PopupGuide.vue
+++ src/components/PopupGuide.vue
@@ -7,7 +7,7 @@ const firstCard = props.stepInfo.cards[0];
 </script>
 
 <template>
-    <div class="flex flex-col mx-8 my-4 items-center justify-center bg-[#EBF7FD] p-6 rounded-xl shadow-lg text-center">
+    <div v-if="firstCard" class="flex flex-col mx-8 my-4 items-center justify-center bg-[#EBF7FD] p-6 rounded-xl shadow-lg text-center">
         <p class="mb-3">{{ firstCard.preTitle }}</p>
         <h2 class="text-xl font-bold mb-4 text-[#009DE0] font-title">{{ firstCard.title }}</h2>
         <div v-for="(paragraph, idx) in firstCard.message" :key="idx" class="mb-3">
